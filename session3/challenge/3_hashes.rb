40b4f0735cbeeb02b671b031ba696ab9e6936524
Kavita K <kkavita92@gmail.com>
2017-04-12 15:59:24 +0800

    Adds solutions for Challenge 3

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,11 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+def word_count(str)
+	list  = Hash.new { |this_hash,key| this_hash[key] = 0 } #block is invoked when a missing key is requested (new word in sentence) 
+	str.split(" ").each do |word|
+		list[word.downcase] += 1
+	end 
+end 
+
